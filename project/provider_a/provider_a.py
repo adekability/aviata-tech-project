548e9da0f7839bff616b12ba387e7f9bfa86c135
Adil <43477533+adekability@users.noreply.github.com>
2023-02-08 21:08:37 +0600

    add: time sleep on provider_a and provider_b

--- project/provider_a/provider_a.py
+++ project/provider_a/provider_a.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+import time
 
 provider_a_bp = Blueprint(
     "provider_a", __name__
@@ -7,4 +8,5 @@ provider_a_bp = Blueprint(
 
 @provider_a_bp.route("/search", methods=["POST"])
 def search_by_provider_a():
+    time.sleep(30)
     return {"status": "provider_a"}
--- project/provider_b/provider_b.py
+++ project/provider_b/provider_b.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+import time
 
 provider_b_bp = Blueprint(
     "provider_b", __name__
@@ -7,4 +8,5 @@ provider_b_bp = Blueprint(
 
 @provider_b_bp.route("/search", methods=["POST"])
 def search_by_provider_b():
+    time.sleep(60)
     return {"status": "provider_b"}
