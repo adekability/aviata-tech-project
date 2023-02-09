8b33c9046ba60043c0adb88789299be55dcf3ffa
Adil <43477533+adekability@users.noreply.github.com>
2023-02-10 04:41:59 +0600

    add: convert method to CurrencyRate model; add: serialize_by_currency method to SearchResult

--- models.py
+++ models.py
@@ -2,6 +2,8 @@ from extensions import Base
 from uuid import uuid4
 from sqlalchemy import *
 from sqlalchemy.dialects.postgresql import DOUBLE_PRECISION, JSON
+import copy
+from datetime import datetime
 
 
 class CurrencyRate(Base):
@@ -21,12 +23,26 @@ class CurrencyRate(Base):
         self.description = description
         self.date = date
 
-    @property
     def serialize(self):
         return dict(id=self.id,
                     fullname=self.fullname,
                     title=self.title,
                     description=self.description)
+
+    @classmethod
+    def convert(cls, from_: dict, to_: dict):
+        from_currency = cls.query.filter_by(title=from_['currency'], date=datetime(2023, 1, 1).date()).first()
+        from_price = from_['price']
+        if to_['currency'] == 'KZT':
+            to_currency = 1
+        else:
+            to_currency = cls.query.filter_by(title=to_['currency'], date=datetime(2021, 1, 1).date()).first()
+            to_currency = to_currency.description
+
+        from_total_kz = from_price * from_currency.description
+
+        result = from_total_kz/to_currency
+        return round(result, 2)
 
 
 class SearchResult(Base):
@@ -45,9 +61,15 @@ class SearchResult(Base):
         self.status = status
         self.items = items
 
-    @property
-    def serialize(self):
-        return dict(id=self.id,
-                    search_id=self.search_id,
+    def serialize_by_currency(self, currency):
+        items = copy.deepcopy(self.items)
+        for item in items:
+            item['price'] = {"amount": CurrencyRate.convert(from_={"currency": item['pricing']['currency'],
+                                                                   "price": float(item['pricing']['total'])},
+                                                            to_={"currency": currency}),
+                             "currency": currency}
+        items = sorted(items, key=lambda x: x['price']['amount'])
+        return dict(search_id=self.search_id,
                     status=self.status,
-                    items=self.items)
+                    items=items)
+
