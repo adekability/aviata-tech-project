e60a465c89dd737c65438b04b3d2dd39349a57bd
Adil <43477533+adekability@users.noreply.github.com>
2023-02-09 10:27:57 +0600

    add: json readers

--- project/provider_a/provider_a.py
+++ project/provider_a/provider_a.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 import time
+import json
 
 provider_a_bp = Blueprint(
     "provider_a", __name__
@@ -9,4 +10,6 @@ provider_a_bp = Blueprint(
 @provider_a_bp.route("/search", methods=["POST"])
 def search_by_provider_a():
     time.sleep(30)
-    return {"status": "provider_a"}
+    provider_a_text = open("./files/response_a.json")
+    provider_a_data = json.load(provider_a_text)
+    return provider_a_data
--- project/provider_b/provider_b.py
+++ project/provider_b/provider_b.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 import time
+import json
 
 provider_b_bp = Blueprint(
     "provider_b", __name__
@@ -9,4 +10,6 @@ provider_b_bp = Blueprint(
 @provider_b_bp.route("/search", methods=["POST"])
 def search_by_provider_b():
     time.sleep(60)
-    return {"status": "provider_b"}
+    provider_b_text = open("./files/response_b.json")
+    provider_b_data = json.load(provider_b_text)
+    return provider_b_data
