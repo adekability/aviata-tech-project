93d5a74d0d2d82feae907ec2b073a1c3c98d7567
Adil <43477533+adekability@users.noreply.github.com>
2023-02-09 23:21:44 +0600

    add: search result model

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 from extensions import db
 from extensions import Base
+from sqlalchemy.dialects.postgresql import JSON
+from uuid import uuid4
 
 
 class CurrencyRate(db.Model, Base):
@@ -22,3 +24,23 @@ class CurrencyRate(db.Model, Base):
                     fullname=self.fullname,
                     title=self.title,
                     description=self.description)
+
+
+class SearchResult(db.Model, Base):
+    __tablename__ = 'search_results'
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    search_id = db.Column(db.String)
+    status = db.Column(db.String)
+    items = db.Column(JSON)
+
+    def __init__(self, search_id=None, status=None, items=None):
+        self.search_id = search_id if search_id else str(uuid4())
+        self.status = status
+        self.items = items
+
+    @property
+    def serialize(self):
+        return dict(id=self.id,
+                    search_id=self.search_id,
+                    status=self.status,
+                    items=self.items)
