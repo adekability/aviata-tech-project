101e9e451f8e3850952a075666a2a205d499d652
Adil <43477533+adekability@users.noreply.github.com>
2023-02-09 21:52:26 +0600

    add: celery configs

--- config.py
+++ config.py
@@ -9,3 +9,7 @@ DB_PASS = os.getenv('DB_PASS')
 DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
 SQLALCHEMY_DATABASE_URI = 'postgresql://'+DB_USER+':'+DB_PASS+'@'+DB_HOST+":"+DB_PORT+'/'+DB_NAME+'?client_encoding=utf-8'
+CELERY_BROKER = os.getenv('CELERY_BROKER')
+CELERY_BACKEND = os.getenv('CELERY_BACKEND')
+CELERY_CONFIG = {"timezone": "Asia/Almaty"}
+CPU_COUNT = os.cpu_count()
