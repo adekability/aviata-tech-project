2d80017b849487bfbef2abb06e7bbfcc149d3f7f
Adil <43477533+adekability@users.noreply.github.com>
2023-02-10 14:53:21 +0600

    add: containerize application

--- Dockerfile
+++ Dockerfile
@@ -11,3 +11,5 @@ COPY requirements.txt ./
 RUN pip install -r requirements.txt
 
 COPY . .
+
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "9000"]
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from routers import provider_a, provider_b, airflow
-import uvicorn
+
 
 app = FastAPI()
 
@@ -8,6 +8,3 @@ app = FastAPI()
 app.include_router(provider_a.router)
 app.include_router(provider_b.router)
 app.include_router(airflow.router)
-
-if __name__ == '__main__':
-    uvicorn.run(app, port=9000)
