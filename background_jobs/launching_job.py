21a08bf225ac9d34f9488aa9392eddc500355485
Adil <43477533+adekability@users.noreply.github.com>
2023-02-10 13:54:34 +0600

    add: celery job on start

--- background_jobs/launching_job.py
+++ background_jobs/launching_job.py
@@ -1,7 +1,14 @@
 from jobs.sync_currencies import SyncCurrency
 from background_jobs.taskmaster import tasks
+from celery.signals import worker_ready
 
 
 @tasks.task
 def launching_job_():
     SyncCurrency.sync_last_five_years()
+
+
+@worker_ready.connect
+def at_start(sender, **k):
+    with sender.app.connection():
+        sender.app.send_task('background_jobs.launching_job.launching_job_')
